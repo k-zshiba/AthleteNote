ab82d3ad53e0a594f715a2408884347c9cb44fe3
shiba <kou480929@gmail.com>
2020-09-10 16:42:35 +0900

    add connecting part

--- php/register.php
+++ php/register.php
@@ -7,4 +7,21 @@ $user_id = $_POST['userID'];
 $password = $_POST['password'];
 $check_password = $_POST['passwordCheck'];
 
-?>
+//データベースに接続
+try
+{
+  $dbh = new PDO(DSN,DB_USER,DB_PASS);
+
+  $sql = "SELECT * FROM `userdata` WHERE 1;";
+  
+  $data = $dbh->query($sql);
+
+  echo $data;
+}
+catch(PDOException $e)
+{
+  exit('データベース接続失敗。'.$e->getMessage());
+}
+
+echo $data;
+// データベースにユーザIDをパスワードを登録
