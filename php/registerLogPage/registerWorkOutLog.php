299c33aa340eeb5b1c72d030f41079ecd90d6a85
shiba <kou480929@gmail.com>
2020-09-12 20:24:51 +0900

    create registerWorkOutLog.php

---
+++ php/registerLogPage/registerWorkOutLog.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lamg="ja">
+<head>
+  <meta charset="utf-8">
+  <title>ログイン画面</title>
+
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+</head>
+<body>
+  <h2>練習登録</h2>
+
+</body>
+</html>
