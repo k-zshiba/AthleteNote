64abdabd8a2836b6cfd3f8ec5fa47daa3324c178
shiba <kou480929@gmail.com>
2020-09-13 22:57:05 +0900

    add form

--- php/registerLogPage/registerPhysicalConditionLog.php
+++ php/registerLogPage/registerPhysicalConditionLog.php
@@ -1,5 +1,5 @@
 <?php
-require_once('dbConfig.php');
+// require_once('.\dbConfig.php');
 
 session_start();
 
@@ -17,23 +17,25 @@ session_start();
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 </head>
 <body>
-  <h2>練習登録</h2>
+  <h2>体調登録</h2>
   <form action="" method = "POST">
     日付<br>
     <input type="date" name = "date"><br>
-    強度<br>
-    <input type="number" name = "intensity" list = "intensity" min = "1" max = "5"><br>
-      <datalist id="intensity">
+    疲労度<br>
+    <input type="number" name = "fatigue" list = "fatigue" min = "1" max = "5"><br>
+      <datalist id="fatigue">
       <option value=1>
       <option value=2>
       <option value=3>
       <option value=4>
       <option value=5>
       </datalist>  
-    感想・意識<br>
-    <textarea name="thought" rows="5" cols="40">ここに感想を記入してください。</textarea><br>
-    画像・動画<br>
-    <input type="file" name = "content" multiple><br>
+    体重<br>
+    <input type="number" value = "50" name = "bodyweight"  step = "0.1">kg<br>  
+    体温<br>
+    <input type="number" value = "35.0" name = "bodytemperature" step = "0.1">℃<br>
+    睡眠時間<br>
+    <input type="number" value = "6" name = "sleeptime" step = "0.5">時間<br> 
     <input type="submit" value = "登録する">
   </form>
 
