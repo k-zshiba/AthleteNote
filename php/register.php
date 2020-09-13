ca03e8ba69551548b36813810d5e1f2410289ff0
shiba <kou480929@gmail.com>
2020-09-13 22:44:24 +0900

    fix validation part

--- php/register.php
+++ php/register.php
@@ -11,45 +11,58 @@ $error_message = "";
 
   if(empty($_POST['userID'])){
     $error_message = 'ユーザIDが入力されていません。';
-    echo $error_message;
-  }
-  if(empty($_POST['password'])){
-    $error_message = 'パスワードが入力されていません。';
     echo $error_message,'<button type="button" onclick=history.back()>戻る</button>';
   }
-  if(empty($_POST['passwordCheck'])){
-    $error_message = '確認用パスワードが入力されていません。';
+  if(empty($_POST['password'])||empty($_POST['passwordCheck'])){
+    $error_message = 'パスワードもしくは確認用パスワードが入力されていません。';
     echo $error_message,'<button type="button" onclick=history.back()>戻る</button>';
-  }
-  if($_POST['password'] != $_POST['passwordCheck']){
-    $error_message = 'パスワードが一致しません。';
-    echo $error_message,'<button type="button" onclick=history.back()>戻る</button>';
-  }
-
-  if(!empty($_POST['userID'])&&!empty($_POST['password'])&&!empty($_POST['passwordCheck'])&&$_POST['password'] == $_POST['passwordCheck']){
-    $user_id = $_POST['userID'];
-    $password = $_POST['password'];
-    $check_password = $_POST['passwordCheck'];
-    //データベースに接続
-    try {
-      $pdo = new PDO(DSN, DB_USER, DB_PASS);
-    } catch (PDOException $e){
-      exit('データベース接続失敗。'.$e->getMessage());
+  }else{
+    if($_POST['password'] != $_POST['passwordCheck']){
+      $error_message = 'パスワードが一致しません。';
+      echo $error_message,'<button type="button" onclick=history.back()>戻る</button>';
     }
 
-
-
-
-    // データベースにユーザID、パスワードを登録
-    try {
-      $stmt = $pdo->prepare("INSERT INTO userdata(userID, password) VALUE (?,?)");
-      $stmt->execute([$user_id, $password]);
-      echo '登録完了';
-    } catch (\Exception $e){
-      echo 'このユーザIDは登録されています。';
-      return FALSE;
+    if(!empty($_POST['userID'])&&$_POST['password'] == $_POST['passwordCheck']){
+      $user_id = $_POST['userID'];
+      $password = $_POST['password'];
+      $check_password = $_POST['passwordCheck'];
+      //データベースに接続
+      try {
+        $pdo = new PDO(DSN, DB_USER, DB_PASS);
+      } catch (PDOException $e){
+        exit('データベース接続失敗。'.$e->getMessage());
+      }
+  
+  
+  
+  
+      // データベースにユーザID、パスワードを登録
+      try {
+        $stmt = $pdo->prepare("SELECT * FROM userdata where userID = ?");
+        $stmt->execute([$_POST['userID']]);
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
+        if(!isset($row['userID'])){
+          try {
+            $stmt = $pdo->prepare("INSERT INTO userdata(userID, password) VALUE (?,?)");
+            $stmt->execute([$user_id, $password]);
+            session_regenerate_id(true); //session_idを新しく生成し、置き換える
+            $_SESSION['userID'] = $user_id;
+            $stmt = null;
+            $pdo = null;
+            header('Location: topPage.php');
+          } catch (\Exception $e){
+            exit($e->getMessage());
+          }
+        }else{
+          exit('このユーザIDは既に登録されています。<button type="button" onclick=history.back()>戻る</button>');
+          
+        }
+      } catch (\Exception $e){
+        exit($e->getMessage()); 
+      }
     }
   }
+
 // ajaxでかく
 
 
