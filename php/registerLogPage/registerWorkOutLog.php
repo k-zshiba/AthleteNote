78852ec8bdad92a4f0ed9967f235897729e0445c
shiba <kou480929@gmail.com>
2020-09-13 22:46:50 +0900

    add form

--- php/registerLogPage/registerWorkOutLog.php
+++ php/registerLogPage/registerWorkOutLog.php
@@ -1,3 +1,11 @@
+<?php
+require_once('dbConfig.php');
+
+session_start();
+
+
+?>
+
 <!DOCTYPE html>
 <html lamg="ja">
 <head>
@@ -8,6 +16,23 @@
 </head>
 <body>
   <h2>練習登録</h2>
-
+  <form action="" method = "POST">
+    日付<br>
+    <input type="date" name = "date"><br>
+    強度<br>
+    <input type="number" name = "intensity" list = "intensity" min = "1" max = "5"><br>
+      <datalist id="intensity">
+      <option value=1>
+      <option value=2>
+      <option value=3>
+      <option value=4>
+      <option value=5>
+      </datalist>  
+    感想・意識<br>
+    <textarea name="thought" rows="5" cols="40">ここに感想を記入してください。</textarea><br>
+    画像・動画<br>
+    <input type="file" name = "content" multiple><br>
+    <input type="submit" value = "登録する">
+  </form>
 </body>
 </html>
