0e5d48189bdc0e03c528db45c0a28de4ae08a876
shiba <kou480929@gmail.com>
2020-09-15 23:05:49 +0900

    add register physical conditon part

--- php/registerLogPage/registerPhysicalConditionLog.php
+++ php/registerLogPage/registerPhysicalConditionLog.php
@@ -1,9 +1,36 @@
 <?php
-// require_once('.\dbConfig.php');
+require_once('..\dbConfig.php');
 
 session_start();
 
+$error_message = "";
 
+if(empty($_POST['date'])){
+  $error_message = "登録日を入力してください。";
+  echo $error_message;
+}else{
+  $fatigue = $_POST['fatigue'];
+  $date = $_POST['date'];
+  $bodyweight = $_POST['bodyweight'];
+  $bodytemperature = $_POST['bodytemperature'];
+  $sleeptime = $_POST['sleeptime'];
+
+
+  // data base接続
+  try {
+    $pdo = new PDO(DSN, DB_USER, DB_PASS);
+  } catch(PDOException $e) {
+    exit('データベース接続失敗。'.$e->getMessage());
+  }
+
+  try {
+    $stmt = $pdo->prepare("INSERT INTO physicalconditionlog(userID, date, fatigue, bodyweight, bodytemperature, sleeptime) value(?,?,?,?,?,?)");
+    $stmt->execute([$_SESSION['userID'], $date,$fatigue,$bodyweight,$bodytemperature,$sleeptime]);
+    header('Location: successRegister.php');
+  } catch(PDOException $e) {
+    exit('データベース接続失敗。'.$e->getMessage());
+  }
+}
 ?>
 
 
@@ -12,14 +39,14 @@ session_start();
 <html lamg="ja">
 <head>
   <meta charset="utf-8">
-  <title>ログイン画面</title>
+  <title>体調登録</title>
 
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 </head>
 <body>
   <h2>体調登録</h2>
   <form action="" method = "POST">
-    日付<br>
+    日付<span> 必須</span><br>
     <input type="date" name = "date"><br>
     疲労度<br>
     <input type="number" name = "fatigue" list = "fatigue" min = "1" max = "5"><br>
