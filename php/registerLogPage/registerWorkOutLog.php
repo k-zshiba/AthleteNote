d638706db84338953c865d5d7dfd6e592a5e8ab1
shiba <kou480929@gmail.com>
2020-09-18 23:06:16 +0900

    add the case of not login

--- php/registerLogPage/registerWorkOutLog.php
+++ php/registerLogPage/registerWorkOutLog.php
@@ -1,7 +1,12 @@
 <?php
+session_start();
+
 require_once('..\dbConfig.php');
 
-session_start();
+if(!isset($_SESSION['userID'])){
+  header("Location: loginPage.php");
+  exit;
+}
 
 $user_id = $_SESSION['userID'];
 $error_message = "";
