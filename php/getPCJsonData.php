7559af064c16b87757049dcded94b95a34d24495
shiba <kou480929@gmail.com>
2020-10-17 16:11:00 +0900

    add getting all pc list and echo json file part

--- php/getPCJsonData.php
+++ php/getPCJsonData.php
@@ -1,5 +1,12 @@
 <?php
 
+session_start();
+
+if(!isset($_SESSION['userID'])){
+    header("Location: loginPage.php");
+    exit;
+}
+
 require_once('./dbConfig.php');
 require_once('./htmlSpecialChars.php');
 require_once('./getFirstDayOfTheWeek.php');
@@ -11,33 +18,30 @@ $last_day_of_the_week = date('Y-m-d',strtotime($first_day_of_the_week. "next Sun
 // $current_page = 1;
 
 // $start_no = ($current_page - 1) * $max_per_page;
-// // echo $current_page;
-// try {
-//     $pdo = new PDO(DSN, DB_USER, DB_PASS);
-//     $sql = 'SELECT * FROM user';
-//     $stmt = $pdo->prepare($sql);
-//     $stmt->execute();
-//     $row = $stmt->fetchAll(PDO::FETCH_ASSOC);
-//     $toral_page = ceil(count($row)/$max_per_page);
-//     $sql = 'SELECT * FROM user LIMIT :start,:end';
-//     $stmt = $pdo->prepare($sql);
-//     $stmt->bindParam(':start', $start_no, PDO::PARAM_INT);
-//     $stmt->bindParam(':end', $max_per_page, PDO::PARAM_INT);
-//     $stmt->execute();
-//     $row = $stmt->fetchAll(PDO::FETCH_ASSOC);
-//     // $row = $stmt->fetchAll();
-//     $json_file = array();
-//     $json_file['total_page'] = $toral_page;
-//     $json_file['current_page_contents'] = $row;
-//     // $json_file = $row;
-//     $json_encoded_file = json_encode($json_file,JSON_UNESCAPED_UNICODE);
-//     header('Content-type: application/json');
-//     echo $json_encoded_file;
-//     $pdo = null;
-//     $stmt = null;
-// }catch (Exception $e) {
-//     $e->getMessage();
-// }
+// echo $current_page;
+try {
+    $pdo = new PDO(DSN, DB_USER, DB_PASS);
+    $sql = 'SELECT * FROM physicalconditionlog WHERE userID = ? AND date BETWEEN ? AND ?';
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute([$_SESSION['userID'],$first_day_of_the_week,$last_day_of_the_week]);
+    $row = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    // var_dump($row);
+    $json_file = array('fatigue'=>[],'date'=>[],'bodyweight'=>[],'bodytemperature'=>[],'sleeptime'=>[]);
+    foreach ($row as $output) {
+    array_push($json_file['fatigue'],$output['fatigue']);
+    array_push($json_file['date'],$output['date']);
+    array_push($json_file['bodyweight'],$output['bodyweight']);
+    array_push($json_file['bodytemperature'],$output['bodytemperature']);
+    array_push($json_file['sleeptime'],$output['sleeptime']);
+    }
+    $json_encoded_file = json_encode($json_file,JSON_UNESCAPED_UNICODE);
+    header('Content-type: application/json');
+    echo $json_encoded_file;
+    $pdo = null;
+    $stmt = null;
+}catch (Exception $e) {
+    $e->getMessage();
+}
 ?>
 
 
