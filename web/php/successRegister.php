8acdd3d3810c957df176050de5124d555bcf5333
k-zshiba <kou480929@gmail.com>
2020-10-17 21:51:25 +0900

    fix included file name

--- web/php/successRegister.php
+++ web/php/successRegister.php
@@ -2,7 +2,7 @@
 session_start();
 
 if(!isset($_SESSION['userID'])){
-  header("Location: .\loginPage.php");
+  header("Location: ..\loginPage.html");
   exit;
 }
 echo '登録完了', '<a href=".\topPage.php">トップ画面へ戻る</a>';
