4459b3227919c17e3acb9d024730949ab15cdbaf
k-zshiba <kou480929@gmail.com>
2020-10-17 21:38:53 +0900

    move folder: php,javascript and ContentsFolder

--- php/dbConfig.php
+++
@@ -1,6 +0,0 @@
-<?php
-$db = parse_url($_SERVER['CLEARDB_DATABASE_URL']);
-$db =['dbName'] = ltrim($db['path'],'/');
-define('DSN',"mysql:host={$db['host']};dbname={$db['host']};charset=utf8");
-define('DB_USER',$db['user']);
-define('DB_PASS',$db['pass']);
