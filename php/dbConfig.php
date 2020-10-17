0b0c2c7e2d07879da5a0abb8380dfcdd30bef752
k-zshiba <kou480929@gmail.com>
2020-10-17 21:13:08 +0900

    change db status

--- php/dbConfig.php
+++ php/dbConfig.php
@@ -1,5 +1,6 @@
 <?php
-
-define('DSN','mysql:host=localhost;dbname=athletenote');
-define('DB_USER','root');
-define('DB_PASS','TfxkC1YNa3bjwbWL');
+$db = parse_url($_SERVER['CLEARDB_DATABASE_URL']);
+$db =['dbName'] = ltrim($db['path'],'/');
+define('DSN',"mysql:host={$db['host']};dbname={$db['host']};charset=utf8");
+define('DB_USER',$db['user']);
+define('DB_PASS',$db['pass']);
