a59a5afda8e00d59926229d841bfe0b11d86a974
k-zshiba <kou480929@gmail.com>
2020-10-24 00:15:03 +0900

    arrange style

--- web/php/signUpPage.php
+++ web/php/signUpPage.php
@@ -5,33 +5,56 @@
   <title>登録画面</title>
 
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.1/css/all.css" integrity="sha384-vp86vTRFVJgpjF9jiIGPEEqYqlDwgyBgEF109VFjmqGmIY/Y4HV4d3Gp2irVfcrp" crossorigin="anonymous">
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+  <!-- Bootstrap Javascript -->
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
 </head>
 <body>
-  <h2>登録はこちら</h2>
-        <form name="register" action="./register.php" method="POST">
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand text-primary" href="../index.html">Athlete Note</a>
+    <ul class="navbar-nav">
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          <i class="fas fa-sliders-h"></i>
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+          <a class="dropdown-item" href="./loginPage.php">ログイン</a>
+          <a class="dropdown-item" href="./signUpPage.php">サインアップ</a>
+        </div>
+      </li>
+    </ul>
+  </nav>
+  <div class="mx-auto col-md-6 col-sm-12">
+    <h2>登録はこちら</h2>
+    <form name="register" action="./register.php" method="POST">
+      <div class="form-group">
+      <label for="InputUserID">ユーザID</label>
+      <input type="text" name ="userID" class="form-control" id="InputUserID" aria-describedby="IDHelp" placeholder="Enter user ID">
+      <small id="IDHelp" class="form-text text-muted">IDを半角数字10桁で入力してください</small>
+      </div>
+      <div class="form-group">
+        <label for="InputPassword">Password</label>
+        <input type="password" name ="password" class="form-control" id="InputPassword" placeholder="Password">
+      </div>
+      <div class="form-group">
+        <input type="password" name ="passwordCheck" class="form-control" id="InputPasswordCheck" placeholder="confirm password">
+        <small id="InputPasswordCheck" class="form-text text-danger">確認用パスワード
+        </small>
+      </div>
+      <button type="submit" class="btn btn-success">登録</button>
+    </form>
+    <h2>登録せずに使う</h2>
+    <form name="guestLogin" action="./guestLogin.php">
+      <button type="submit" class="btn btn-primary">ゲストログイン</button>
+    </form>
+  </div>
 
 
 
-            <label for="InputUserID">ID</label><br>
-            <input type="text" name ="userID" id="InputUserID" aria-describedby="IDHelp">
-            <small id="IDHelp">IDを半角数字10桁で入力してください</small><br>
-
-
-
-            <label for="InputPassword">Password</label><br>
-            <input type="password" name ="password" id="InputPassword">
-            <small id="passwordHelp">パスワードを入力してください</small><br>
-
-
-            <label for="InputPasswordCheck">確認用Password</label><br>
-            <input type="password" name ="passwordCheck" id="InputPasswordCheck">
-            <small id="passwordCheckHelp">もう一度パスワードを入力してください</small><br>
-
-            <button type="submit" id = "signUp">登録</button>
-        </form>
-        <h2>登録せずに使う</h2>
-        <form name="guestLogin" action="./guestLogin.php">
-          <button type="submit">ゲストログイン</button>
-        </form>
 </body>
 </html>
