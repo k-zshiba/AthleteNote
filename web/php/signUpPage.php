664bc9053ba7e9abbc8a0577d7f37b0a6ba4f647
k-zshiba <kou480929@gmail.com>
2020-10-23 22:12:36 +0900

    add register form

---
+++ web/php/signUpPage.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lamg="ja">
+<head>
+  <meta charset="utf-8">
+  <title>登録画面</title>
+
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+</head>
+<body>
+  <h2>初めての方はこちら</h2>
+        <form name="register" action="./register.php" method="POST">
+
+
+
+            <label for="InputUserID">ID</label><br>
+            <input type="text" name ="userID" id="InputUserID" aria-describedby="IDHelp">
+            <small id="IDHelp">IDを半角数字10桁で入力してください</small><br>
+
+
+
+            <label for="InputPassword">Password</label><br>
+            <input type="password" name ="password" id="InputPassword">
+            <small id="passwordHelp">パスワードを入力してください</small><br>
+
+
+            <label for="InputPasswordCheck">確認用Password</label><br>
+            <input type="password" name ="passwordCheck" id="InputPasswordCheck">
+            <small id="passwordCheckHelp">もう一度パスワードを入力してください</small><br>
+
+            <button type="submit" id = "signUp">登録</button>
+        </form>
+</body>
+</html>
