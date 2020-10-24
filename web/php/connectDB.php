80aca42fbe40915ad1c3f41b1a6fe01ea75cb453
k-zshiba <kou480929@gmail.com>
2020-10-25 00:11:31 +0900

    change db status

--- web/php/connectDB.php
+++ web/php/connectDB.php
@@ -1,16 +1,16 @@
 <?php
 function connectDB () {
-    // $url = getenv('JAWSDB_URL');
-    // $db = parse_url($url);
-    // $db['dbName'] = ltrim($db['path'],'/');
+    $url = getenv('JAWSDB_URL');
+    $db = parse_url($url);
+    $db['dbName'] = ltrim($db['path'],'/');
 
-    // $dsn = "mysql:host={$db['host']};dbname={$db['dbName']};charset=utf8";
-    // $db_user = $db['user'];
-    // $db_pass = $db['pass'];
+    $dsn = "mysql:host={$db['host']};dbname={$db['dbName']};charset=utf8";
+    $db_user = $db['user'];
+    $db_pass = $db['pass'];
 
-    $dsn = 'mysql:host=localhost;dbname=athletenote';
-    $db_user = 'root';
-    $db_pass = 'TfxkC1YNa3bjwbWL';
+    // $dsn = 'mysql:host=localhost;dbname=athletenote';
+    // $db_user = 'root';
+    // $db_pass = 'TfxkC1YNa3bjwbWL';
     $driver_options = [
       PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
       PDO::ATTR_EMULATE_PREPARES => false,
