4d016ef53fc9ab288dd5de989b6041b54545e0d4
k-zshiba <kou480929@gmail.com>
2020-10-25 21:57:59 +0900

    create twitterConfig.php

---
+++ web/php/twitterConfig.php
@@ -0,0 +1,5 @@
+<?php
+define('OAUTH_CONSUMER_KEY','zBVvOxBvyM8OZ5tSKzHpOQXpq');
+define('OAUTH_CONSUMER_SECRET','17W0w3ErJ4Wld5XoKUlxM9AeyaaAVvqhsoRgNUEg6OLilIaLtj');
+$callback_url = 'https://athlete-note.herokuapp.com/php/twitterCallback.php';
+
