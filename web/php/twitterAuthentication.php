882839f1bbae10fcdd38e55702ca1a1b283c1e01
k-zshiba <kou480929@gmail.com>
2020-10-25 23:37:13 +0900

    fix typo

--- web/php/twitterAuthentication.php
+++ web/php/twitterAuthentication.php
@@ -7,7 +7,7 @@ use Abraham\TwitterOAuth\TwitterOAuth;
 
 $twitter_connection = new TwitterOAuth(OAUTH_CONSUMER_KEY,OAUTH_CONSUMER_SECRET);
 
-$twitter_request_token = post('oauth/request_token',['oauth_callback'=>$callback_url]);
+$twitter_request_token = $twitter_connection->oauth('oauth/request_token',['oauth_callback'=>$callback_url]);
 
 $_SESSION['oauth_token'] = $twitter_request_token['oauth_token'];
 $_SESSION['oauth_token_secret'] = $twitter_request_token['oauth_token_secret'];
