b582eae4ca80eef0eed964f43faed152d614c1b3
k-zshiba <kou480929@gmail.com>
2020-10-26 21:33:45 +0900

    fix typo

--- web/php/twitterCallback.php
+++ web/php/twitterCallback.php
@@ -14,7 +14,7 @@ if (isset($_REQUEST['oauth_token']) && $twitter_request_token['oauth_token'] !==
     exit;
 }else if (isset($_REQUEST['oauth_token']) && $twitter_request_token['oauth_token'] === $_REQUEST['oauth_token']) {
     $twitter_connection = new TwitterOAuth(OAUTH_CONSUMER_KEY,OAUTH_CONSUMER_SECRET,$twitter_request_token['oauth_token'],$twitter_request_token['oauth_token_secret']);
-    $access_token = $connection->oauth("oauth/access_token", ["oauth_verifier" => $_REQUEST['oauth_verifier']]);
+    $access_token = $twitter_connection->oauth("oauth/access_token", ["oauth_verifier" => $_REQUEST['oauth_verifier']]);
     $_SESSION['access_token'] = $access_token;
     session_regenerate_id();
     header('location: ./tweetWorkOutLog.php');
