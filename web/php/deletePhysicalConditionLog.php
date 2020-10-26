cdf37bd060b75c72045447bf432004fdf7ea4a03
k-zshiba <kou480929@gmail.com>
2020-10-26 21:50:48 +0900

    change style

--- web/php/deletePhysicalConditionLog.php
+++ web/php/deletePhysicalConditionLog.php
@@ -13,11 +13,6 @@ try {
     $stmt = $pdo->prepare("DELETE FROM physicalconditionlog WHERE physicalconditionlogID = ? AND userID = ?");
     $stmt->execute([$physical_condition_log_id, $_SESSION['userID']]);
     $result = $stmt->rowCount();
-    if ($result) {
-        echo '削除に成功しました。<br>'.'<button type="button" onclick="location.href = \'./watchPCLog.php\'">体調ログ一覧に戻る</button>';
-    }else {
-        echo '削除に失敗しました。<br>'.'<button type="button" onclick="location.href = \'./watchPCLog.php\'">体調ログ一覧に戻る</button>';
-    }
 }catch (PDOExeption $e) {
     exit($e->getMessage());
 }
@@ -26,11 +21,42 @@ try {
 <html lang="ja">
 <head>
   <meta charset="UTF-8">
-  <title>Delete</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.1/css/all.css" integrity="sha384-vp86vTRFVJgpjF9jiIGPEEqYqlDwgyBgEF109VFjmqGmIY/Y4HV4d3Gp2irVfcrp" crossorigin="anonymous">
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+  <!-- Bootstrap Javascript -->
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+  <title>削除確認画面</title>
 </head>
-
 <body>
-
-  <button type="button" onclick="location.href='./topPage.php'">トップに戻る</button>
-</body>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand text-primary" href="../index.html">Athlete Note</a>
+    <ul class="navbar-nav">
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          <i class="fas fa-sliders-h"></i>
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+          <a class="dropdown-item" href="./loginPage.php">ログイン</a>
+          <a class="dropdown-item" href="./signUpPage.php">サインアップ</a>
+        </div>
+      </li>
+    </ul>
+  </nav>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarDropdownMenuLink" aria-controls="navbarDropdownMenuLink" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="text-center col-12">
+  <?php
+    if ($result) {
+        echo '削除に成功しました。<br>'.'<button type="button" class="btn btn-danger" onclick="location.href = \'./watchPCLog.php\'">体調ログ一覧に戻る</button>';
+    }else {
+        echo '削除に失敗しました。<br>'.'<button type="button" class="btn btn-danger" onclick="location.href = \'./watchPCLog.php\'">体調ログ一覧に戻る</button>';
+    }
+  ?>
+  </div>
 </html>
