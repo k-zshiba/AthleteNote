ba84e19dd3eb7e4bbd369d10efd409ba96cc1ae1
k-zshiba <kou480929@gmail.com>
2020-10-26 21:50:48 +0900

    change style

--- web/php/editPCLog.php
+++ web/php/editPCLog.php
@@ -21,11 +21,6 @@ try{
     $physical_condition_log = array(':fatigue'=>$fatigue, ':date'=>$date,':bodyweight'=>$bodyweight,':bodytemperature'=>$bodytemperature,':sleeptime'=>$sleeptime, ':physicalconditionlogID'=>$physical_condition_log_id,':userID'=>$userID);
     $stmt->execute($physical_condition_log);
     $result = $stmt->rowCount();
-    if ($result) {
-        echo "編集に成功しました。<br>";
-    }else {
-        echo "編集に失敗しました。<br>";
-    }
 }catch (PDOExeption $e) {
     echo $e->getMessage();
 }
@@ -35,11 +30,43 @@ try{
 <html lang="ja">
 <head>
   <meta charset="UTF-8">
-  <title>insert</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.1/css/all.css" integrity="sha384-vp86vTRFVJgpjF9jiIGPEEqYqlDwgyBgEF109VFjmqGmIY/Y4HV4d3Gp2irVfcrp" crossorigin="anonymous">
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+  <!-- Bootstrap Javascript -->
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+  <title>編集確認画面</title>
 </head>
-
 <body>
-
-  <button type="button" onclick="location.href='./watchPCLog.php'">一覧に戻る</button>
-</body>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand text-primary" href="../index.html">Athlete Note</a>
+    <ul class="navbar-nav">
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          <i class="fas fa-sliders-h"></i>
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+          <a class="dropdown-item" href="./loginPage.php">ログイン</a>
+          <a class="dropdown-item" href="./signUpPage.php">サインアップ</a>
+        </div>
+      </li>
+    </ul>
+  </nav>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarDropdownMenuLink" aria-controls="navbarDropdownMenuLink" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="text-center col-12">
+  <?php
+    if ($result) {
+        echo "<h2>編集に成功しました。</h2>";
+    }else {
+        echo "<h2>編集に失敗しました。</h2>";
+    }
+  ?>
+    <button type="button" class="btn btn-danger" onclick="location.href = './topPage.php'">トップに戻る</button>
+  </div>
 </html>
