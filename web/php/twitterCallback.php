787af70f2ddc494adf6d14abcfe139ef79b61541
k-zshiba <kou480929@gmail.com>
2020-10-26 21:23:20 +0900

    fix typo

--- web/php/twitterCallback.php
+++ web/php/twitterCallback.php
@@ -8,13 +8,11 @@ use Abraham\TwitterOAuth\TwitterOAuth;
 $twitter_request_token['oauth_token'] = $_SESSION['oauth_token']; 
 $twitter_request_token['oauth_token_secret'] = $_SESSION['oauth_token_secret'];
 
-var_dump($_REQUEST['oauth_token']);
-var_dump($request_token['oauth_token']);
 
-if (isset($_REQUEST['oauth_token']) && $request_token['oauth_token'] !== $_REQUEST['oauth_token']) {
+if (isset($_REQUEST['oauth_token']) && $twitter_request_token['oauth_token'] !== $_REQUEST['oauth_token']) {
     echo 'Twitterの認証に失敗しました。<br>'.'<button type="button" class="btn btn-danger" onclick="location.href=\'./topPage.php\'">トップに戻る</button>';
     exit;
-}else if (isset($_REQUEST['oauth_token']) && $request_token['oauth_token'] === $_REQUEST['oauth_token']) {
+}else if (isset($_REQUEST['oauth_token']) && $twitter_request_token['oauth_token'] === $_REQUEST['oauth_token']) {
     $twitter_connection = new TwitterOAuth(OAUTH_CONSUMER_KEY,OAUTH_CONSUMER_SECRET,$twitter_request_token['oauth_token'],$twitter_request_token['oauth_token_secret']);
     $access_token = $connection->oauth("oauth/access_token", ["oauth_verifier" => $_REQUEST['oauth_verifier']]);
     $_SESSION['access_token'] = $access_token;
