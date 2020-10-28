583777635482d667eb94e038b6386e3ae4fe939d
k-zshiba <kou480929@gmail.com>
2020-10-29 00:03:02 +0900

    fix value name

--- web/php/twitterConfig.php
+++ web/php/twitterConfig.php
@@ -1,6 +1,6 @@
 <?php
-$oauth_consumer_key = getenv('OAUTH_CONSUMER_KEY');
-$oauth_consumer_secret = getenv('OAUTH_CONSUMER_SECRET');
+$oauth_consumer_key = getenv('TWITTER_OAUTH_CONSUMER_KEY');
+$oauth_consumer_secret = getenv('TWITTER_OAUTH_CONSUMER_SECRET');
 
 define('OAUTH_CONSUMER_KEY',$oauth_consumer_key);
 define('OAUTH_CONSUMER_SECRET',$oauth_consumer_secret);
