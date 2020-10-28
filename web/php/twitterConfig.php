e9556dcee5c82fc4f670bbe542e85360c3ed1ef1
k_zshiba <65855987+k-zshiba@users.noreply.github.com>
2020-10-28 23:55:11 +0900

    Delete twitterConfig.php

--- web/php/twitterConfig.php
+++
@@ -1,8 +0,0 @@
-<?php
-$oauth_consumer_key = getenv('OAUTH_CONSUMER_KEY');
-$oauth_consumer_secret = getenv('OAUTH_CONSUMER_SECRET');
-
-define('OAUTH_CONSUMER_KEY',$oauth_consumer_key);
-define('OAUTH_CONSUMER_SECRET',$oauth_consumer_secret);
-$callback_url = 'https://athlete-note.herokuapp.com/php/twitterCallback.php';
-
